57dd12a7978a7a515baf46151dc4276a64000cc2
[kgramer] <[kyle@gramers.com]>
2022-08-05 22:54:34 -0400

    task 5 kgramer

--- ouline.py
+++ ouline.py
@@ -2,10 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-#function to get job list from url f'https://www.talent.com/jobs?k={role}&l={location}'
 def getJobList(role,location):
-    url = f'https://www.talent.com/jobs?k={role}&l={location}'
-    # Complete the missing part of this function here
+
+    url = "https://www.talent.com/jobs?k="+role+"&l="+location
+
+    response = requests.request("GET", url)
+    print('Response code: '+str(response.status_code))
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    JobDetails = soup.find_all('div', class_='card card__job')
+    myArr = {}
+    i = 0
+    for job in JobDetails:
+        jobTitle = job.find('h2', class_='card__job-title').text.strip()
+        company = job.find('div', class_='card__job-empname-label').text.strip()
+        description = job.find('p', class_='card__job-snippet').text.replace('\n', '').replace("'", "").strip()
+        jobDetailsjson = {
+            "Title": jobTitle,
+            "Company": company,
+            "Description": description
+        }
+        myArr[i] = jobDetailsjson
+        i = i + 1
+    return myArr
+
+
+
+
 
 #save data in JSON file
 def saveDataInJSON(jobDetails):
@@ -20,7 +43,7 @@ def main():
     location = input()
     print("Chosen role: "+role+". Chosen location: "+location)
     print("Searching...")
-    # getJobList(role,location)
+    print(getJobList(role,location))
     
 if __name__ == '__main__':
     main()
